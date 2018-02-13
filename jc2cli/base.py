5ba7d668de509c2e9425c3aeba4161515abaf184
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2018-02-13 09:02:39 -0800

    Create recording features in base class.

--- jc2cli/base.py
+++ jc2cli/base.py
@@ -9,6 +9,7 @@ __docformat__ = 'restructuredtext en'
 #             |_|
 # -----------------------------------------------------------------------------
 #
+import json
 import jc2cli.tools.loggerator as loggerator
 from jc2cli.completer import CliCompleter
 from prompt_toolkit import prompt
@@ -66,6 +67,8 @@ class Base(object):
         self.toolbar_str = ''
         self.prompt_str = "> "
         self.rprompt_str = ''
+        self.__recording = False
+        self.__record_data = []
 
     @property
     def commands(self):
@@ -89,6 +92,114 @@ class Base(object):
             lista = line.split()
             return lista[0], ' '.join(lista[1:])
         return None, None
+
+    def start_recording(self):
+        """Starts recording commands input in the command line.
+
+        Returns:
+            None
+        """
+        self.__recording = True
+
+    def stop_recording(self):
+        """Stops recording commands input in the command line.
+
+        Returns:
+            None
+        """
+        self.__recording = False
+        if self.__record_data:
+            del self.__record_data[-1]
+
+    def clear_recording(self, from_record=None, to_record=None):
+        """Clears the range of records recorded from the given range.
+
+        Args:
+            from_record (int) : First record to clear. Set to 0 if None.
+
+            to_record (int): Last record to clear. Set to last if None
+        """
+        if from_record is None and to_record is None:
+            self.__record_data.clear()
+        elif from_record is None and to_record is not None:
+            if to_record < len(self.__record_data):
+                del self.__record_data[:to_record + 1]
+        elif from_record is not None and to_record is None:
+            if 0 <= from_record <= len(self.__record_data):
+                del self.__record_data[from_record:]
+        elif (0 <= from_record <= len(self.__record_data)) and\
+                to_record < len(self.__record_data) and\
+                from_record <= to_record:
+                del self.__record_data[from_record:to_record + 1]
+        else:
+            pass
+
+    def select_recording(self, from_record=None, to_record=None):
+        """Selects the range of records recorded from the given range.
+
+        Args:
+            from_record (int) : First record to select. Set to 0 if None.
+
+            to_record (int): Last record to select. Set to last if None
+
+        Returns:
+            list : List of selected records.
+        """
+        if from_record is None and to_record is None:
+            return self.__record_data
+        elif from_record is None and to_record is not None:
+            if to_record < len(self.__record_data):
+                return self.__record_data[:to_record + 1]
+        elif from_record is not None and to_record is None:
+            if 0 <= from_record <= len(self.__record_data):
+                return self.__record_data[from_record:]
+        elif (0 <= from_record <= len(self.__record_data)) and\
+                to_record < len(self.__record_data) and\
+                from_record < to_record:
+                return self.__record_data[from_record:to_record + 1]
+        else:
+            return []
+
+    def display_recording(self, from_record=None, to_record=None):
+        """Displays the range of records recorded from the given range.
+
+        Args:
+            from_record (int) : First record to display. Set to 0 if None.
+
+            to_record (int): Last record to display. Set to last if None
+
+        Returns:
+            None
+        """
+        records = self.select_recording(from_record, to_record)
+        for i, record in enumerate(records):
+            logger.display('{0}: {1}'.format(i, record))
+
+    def save_recording(self, filename, from_record=None, to_record=None):
+        """
+        """
+        records = self.select_recording(from_record, to_record)
+        to_save = []
+        for record in records:
+            to_save.append({'command': record})
+        if to_save:
+            with open(filename, 'w') as f:
+                json.dump(to_save, f)
+
+    def record_command(self, user_input):
+        """Saves in a JSON file the range of records recorded from the given
+        range.
+
+        Args:
+            from_record (int) : First record to save. Set to 0 if None.
+
+            to_record (int): Last record to save. Set to last if None
+
+        Returns:
+            None
+        """
+        if self.__recording:
+            self.__record_data.append(user_input)
 
     def get_bottom_toolbar_tokens(self, cli):
         """Method that provides data and format to be displayed in the ToolBar.
@@ -141,6 +252,7 @@ class Base(object):
 
         print('You entered: {}'.format(line))
         command, params = Base.get_command_from_line(line)
+        self.record_command(line)
         result = self.handler(command, line=params)
         if result is None:
             print('Unknown command: {}'.format(line))
